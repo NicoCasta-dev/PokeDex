8bd35c800388167f2b76a125bf52a0fb6095da4e
Nicolas <nicocasta-dev@gmail.com>
2024-12-29 15:09:09 +0100

    Add Pokemon frenchName

--- src/app/models/pokemon.ts
+++ src/app/models/pokemon.ts
@@ -1,6 +1,7 @@
 export interface Pokemon {
     id: number;
     name: string;
+    frenchName: string;
     sprites: {
         front_default: string;
     };
--- src/app/services/pokemon.service.ts
+++ src/app/services/pokemon.service.ts
@@ -23,22 +23,47 @@ export class PokemonService {
 
   constructor(private http: HttpClient) { }
 
+  getPokemonFrenchName(id: number): Observable<string> {
+    return this.http.get<any>(`${this.baseUrl}/pokemon-species/${id}`).pipe(
+      map(data => {
+        const frenchName = data.names.find((name: any) => name.language.name === 'fr');
+        return frenchName ? frenchName.name : data.name;
+      })
+    );
+  }
+
   getPokemonList(offset: number = 0, limit: number = 151): Observable<Pokemon[]> {
-    return this.http.get<PokemonListResponse>(`${this.baseUrl}/pokemon?offset=${offset}&limit=${limit}`).pipe(map(result => result.results),
-
+    return this.http.get<PokemonListResponse>(`${this.baseUrl}/pokemon?offset=${offset}&limit=${limit}`).pipe(
+      map(result => result.results),
       switchMap(pokemons => {
-        const detailRequests: Observable<Pokemon>[] = pokemons.map(pokemon =>
-          this.http.get<Pokemon>(pokemon.url)
+        const detailRequests = pokemons.map(pokemon =>
+          this.http.get<Pokemon>(pokemon.url).pipe(
+            switchMap(pokemonData => 
+              this.getPokemonFrenchName(pokemonData.id).pipe(
+                map(frenchName => ({
+                  ...pokemonData,
+                  frenchName: frenchName
+                }))
+              )
+            )
+          )
         );
-
         return forkJoin(detailRequests);
       })
-    )
-    ;
+    );
   }
 
   getPokemonDetails(id: number): Observable<Pokemon> {
-    return this.http.get<Pokemon>(`${this.baseUrl}/pokemon/${id}`);
+    return this.http.get<Pokemon>(`${this.baseUrl}/pokemon/${id}`).pipe(
+      switchMap(pokemon => 
+        this.getPokemonFrenchName(pokemon.id).pipe(
+          map(frenchName => ({
+            ...pokemon,
+            frenchName: frenchName
+          }))
+        )
+      )
+    );
   }
 
   searchPokemon(name: string): Observable<Pokemon[]> {
